8f0051a3f3ecd4bde86bc463339f18d3aabfb524
foroughgoudarzi <foroughg@yahoo.com>
2024-03-08 10:54:15 +0000

    deploy netlify

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { StaticRouter } from "react-router-dom/server";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
